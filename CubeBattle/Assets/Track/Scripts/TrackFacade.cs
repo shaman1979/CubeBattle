d59eff355753c0ef45e5bc438bd73af39a5fb28d
shaman1979 <upgamestudio@gmail.com>
2020-10-18 21:58:16 +0300

    Добавил дорожки

---
+++ CubeBattle/Assets/Track/Scripts/TrackFacade.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace CubeBattle.Tracks
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class TrackFacade : SerializedMonoBehaviour
+    {
+        [SerializeField]
+        private Transform spawnPoint;
+
+        [SerializeField]
+        private string trackName;
+
+        public Vector3 GetSpawnPosition() => spawnPoint.position;
+
+        private void OnMouseDown()
+        {
+            Debug.Log($"Создание воина на дороге {trackName} по координате {GetSpawnPosition()}");
+        }
+    }
+}
