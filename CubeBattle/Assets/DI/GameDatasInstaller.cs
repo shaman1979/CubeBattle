56c0fb2de5179509e7c97a83e5a2fe5a1ff6f373
shaman1979 <upgamestudio@gmail.com>
2021-02-05 22:10:32 +0300

    Переименовал папку

--- CubeBattle/Assets/DI/GameDatasInstaller.cs
+++ CubeBattle/Assets/DI/GameDatasInstaller.cs
@@ -1,12 +1,20 @@
 using CubeBattle.Units.Datas;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
 
 [CreateAssetMenu(fileName = "GameDatasInstaller", menuName = "Installers/GameDatasInstaller")]
 public class GameDatasInstaller : ScriptableObjectInstaller<GameDatasInstaller>
 {
+    [SerializeField]
+    private string unitDatasPath = "WarriorDatas";
+
     public override void InstallBindings()
     {
-        Container.Bind<UnitData>().FromScriptableObjectResource("WarriorDatas").AsSingle();
+        Container.Bind<IEnumerable<UnitData>>().FromMethod(binder =>
+        {
+            return new List<UnitData>(Resources.LoadAll<UnitData>(unitDatasPath));
+        }).AsCached();
     }
 }
